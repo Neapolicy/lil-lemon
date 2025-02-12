83f54207e9e2a5895e923f538531ff47e3ff973d
neapolicy <matthewlin559@gmail.com>
2025-02-12 12:30:16 -0500

    minor changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,14 @@
 export default function Main(){
-    function handleSubmit(){
-        
+    function handleSubmit(formData){
+        const data = formData.get("date");
+        console.log(data);
     }
 
     return <main>
-        <form>
-            <input type = 'date'></input>
-            <button type = 'submit'>confirm date</button>
+        <form action={handleSubmit} className="form">
+            <input type = 'date' name = 'date'></input>
+            <input type = 'radio' name = 'time'></input>
+            <button type = 'submit'>submit</button>
         </form>
     </main>
 }
